b747d275c150f6979b0726ed6759c500cf841379
saltorious <cryptobet95@gmail.com>
2023-02-01 22:15:06 -0500

    ok

--- src/components/paypal/paypal.js
+++ src/components/paypal/paypal.js
@@ -74,7 +74,6 @@ const Paypal = (props) => {
   const [acceptPolicy, setAcceptPolicy] = useState(false);
 
   const handlePolicyAccept = useCallback((e) => {
-    console.log(pageData)
     e.preventDefault();
     if (!pageData.name || !pageData.email || !pageData.phone) return swal('Error', 'You forgot to enter something!', 'error');
     if (!pageData.scheduledDate) return swal ('Error', 'You forgot to select a date!', 'error');
