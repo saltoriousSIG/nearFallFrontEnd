d23b4a62a0127712565e34c6b209702ec6796d7c
saltorious <cryptobet95@gmail.com>
2023-02-01 22:20:32 -0500

    ok

--- src/utils/trips.js
+++ src/utils/trips.js
@@ -91,7 +91,7 @@ export const trips = {
             maxOccupants: 6
         },
         price: {
-            deposit: 1,
+            deposit: 200,
             balance: 800
         },
         details:'Bring your own Jigs; Clam and Green Crabs provided; White leggers provided when available;',
